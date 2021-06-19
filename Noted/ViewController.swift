5a57122538011b583c0b49b4234aa61d8555c6a2
Ikmal Azman <opikmalazman@gmail.com>
2021-06-19 15:47:32 +0800

    Initial Commit

---
+++ Noted/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Noted
+//
+//  Created by Ikmal Azman on 19/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
