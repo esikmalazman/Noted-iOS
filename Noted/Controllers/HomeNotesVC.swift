bacf153ac027cfe82c37a1398233d1e7dd659213
Ikmal Azman <opikmalazman@gmail.com>
2022-02-19 08:57:32 +0800

    moving out data manipulation method to presenter and set cell to configure function instead of set property directly

--- Noted/Controllers/HomeNotesVC.swift
+++ Noted/Controllers/HomeNotesVC.swift
@@ -16,13 +16,13 @@ final class HomeNotesVC: UIViewController {
     @IBOutlet weak var tableView: UITableView!
 
     // MARK: - Variables
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     private var arrayNotes = [Note]()
+    private let presenter = HomeNotesPresenter()
 
     // MARK: - LifeCycle
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.isNavigationBarHidden = true
-        guard arrayNotes.count < 0  else {return loadNotes()}
+        guard arrayNotes.count < 0  else {return presenter.reloadNotes()}
     }
 
     override func viewWillDisappear(_ animated: Bool) {
@@ -32,10 +32,10 @@ final class HomeNotesVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        presenter.delegate = self
         // Receive notification if new items added
         // NotificationCenter.default.addObserver(self, selector: #selector(loadNotes), name: NSNotification.Name(rawValue: "loadTableView"), object: nil)
-        tableView.register(UINib(nibName: "CustomNotedCell", bundle: nil), forCellReuseIdentifier: Constants.customCellIdentifierNote)
-        loadNotes()
+        presenter.reloadNotes()
     }
 
     @IBAction func addButtonPressed(_ sender: UIButton) {
@@ -57,7 +57,7 @@ extension HomeNotesVC: UITableViewDelegate {
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 
-        guard let destinationVC = segue.destination as? ViewNotesVC else {return}
+        guard let destinationVC = segue.destination as? NotesDetailsVC else {return}
         guard let indexPath = tableView.indexPathForSelectedRow else {return}
 
         destinationVC.notesTitle = arrayNotes[indexPath.row].title
@@ -70,13 +70,13 @@ extension HomeNotesVC: UITableViewDelegate {
     }
 
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        let action = UIContextualAction(style: .destructive, title: "Delete") { (_, _, _) in
+        let action = UIContextualAction(style: .destructive, title: "Delete") { [weak self] (_, _, _)  in
 
-            let notesToRemove = self.arrayNotes[indexPath.row]
+            guard let notesToRemove = self?.arrayNotes[indexPath.row] else {return}
+
+            self?.presenter.deleteNotes(notesToRemove)
+            self?.tableView.reloadData()
             SoundManager.shared.playSound(soundFileName: Constants.SoundFile.deleteNotes)
-            self.context.delete(notesToRemove)
-            self.saveNotes()
-            self.loadNotes()
 
         }
         return UISwipeActionsConfiguration(actions: [action])
@@ -84,7 +84,6 @@ extension HomeNotesVC: UITableViewDelegate {
 }
 
 // MARK: - Table Datasource
-
 extension HomeNotesVC: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if arrayNotes.isEmpty {
@@ -97,56 +96,51 @@ extension HomeNotesVC: UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let listOfNotes = arrayNotes[indexPath.row]
-        // swiftlint:disable force_cast
-        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.customCellIdentifierNote, for: indexPath) as! CustomNotedCell
-        // swiftlint:enable force_cast
 
-        cell.titleCell.text = listOfNotes.title
-        cell.subtitleCell.text = listOfNotes.text
-        cell.cellBg.backgroundColor = listOfNotes.cellColor
-        cell.selectionStyle = .none
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.customCellIdentifierNote, for: indexPath) as! CustomNotedCell
+        cell.configureCell(withData: listOfNotes)
+
         tableView.backgroundView = nil
 
         return cell
     }
 }
 
-// MARK: - Data Manipulation Methods
-extension HomeNotesVC {
-
-    func saveNotes() {
-        do {
-            try context.save()
-        } catch {
-            print("Error in saving context \(error.localizedDescription)")
-        }
-        tableView.reloadData()
-    }
-    @objc func loadNotes() {
-
-        let request: NSFetchRequest<Note> = Note.fetchRequest()
-        do {
-            arrayNotes = try context.fetch(request)
-        } catch {
-            print("Error load context \(error)")
-        }
-        tableView.reloadData()
-    }
-}
-
 // MARK: - Private methods
 extension HomeNotesVC {
     private func setupUI() {
+
+        setupTableView()
+        setupAddButon()
+
         navigationController?.navigationBar.tintColor = Constants.BrandColor.mainFontColor
         view.backgroundColor = Constants.BrandColor.bgColor
-        tableView.backgroundColor = Constants.BrandColor.bgColor
 
         baseTitle.textColor = Constants.BrandColor.mainFontColor
         baseSubtitle.textColor = Constants.BrandColor.mainFontColor
+    }
 
+    private func setupTableView() {
+        tableView.register(UINib(nibName: XIBName.CustomNotedCell.rawValue, bundle: nil), forCellReuseIdentifier: Constants.customCellIdentifierNote)
+        tableView.backgroundColor = Constants.BrandColor.bgColor
+    }
+
+    private func setupAddButon() {
         addButton.backgroundColor = Constants.BrandColor.mainFontColor
         addButton.tintColor = Constants.BrandColor.bgColor
-
         addButton.layer.cornerRadius = 30
+    }
+}
+
+// MARK: - HomeNotesPresenterDelegate
+extension HomeNotesVC: HomeNotesPresenterDelegate {
+    func presentFetchNotesWhenSuccess(_ HomeNotesPresenter: HomeNotesPresenter, data: [Note]) {
+
+        arrayNotes = data
+        tableView.reloadData()
+    }
+    #warning("Add UIAlert here")
+    func presentFetchNotesWithError(_ HomeNotesPresenter: HomeNotesPresenter, message: String) {
+
     }
 }
