f87bbad1f200d035ecf52bf39b80d64bf38b2047
Ikmal Azman <opikmalazman@gmail.com>
2022-02-19 08:43:40 +0800

    Create a core data manager to extract similar data manipulation methods in every vc.

---
+++ Noted/Model/Core Data/CoreDataManager.swift
@@ -0,0 +1,46 @@
+//
+//  CoreDataManager.swift
+//  Noted
+//
+//  Created by Ikmal Azman on 19/02/2022.
+//
+
+import UIKit
+import CoreData
+
+final class CoreDataManager {
+
+    private let context = Constants.accessObjectContext
+
+    func saveObjectContext(_ object: NSManagedObject, completion : @escaping (() -> Void) = {}) {
+        do {
+            try context.save()
+            completion()
+        } catch {
+            print("Error saving object : \(object) into Context with error : \(error.localizedDescription)")
+            completion()
+        }
+    }
+
+    func removeObjectContext(_ object: NSManagedObject) {
+        context.delete(object)
+        self.saveObjectContext(object)
+    }
+
+    func fetchObjectContext<T: NSManagedObject>(_ object: T.Type,
+                                               withPredicate predicate: NSPredicate? = nil,
+                                               completion : @escaping (Result<[NSFetchRequestResult], Error>) -> Void) {
+
+        let request = T.fetchRequest()
+        request.predicate = predicate
+
+        do {
+            let fetchedObject = try context.fetch(request)
+            completion(.success(fetchedObject))
+        } catch {
+            print("Error fetching requested data from Context : \(request), with error : \(error.localizedDescription)")
+            completion(.failure(error.localizedDescription as! Error))
+        }
+
+    }
+}
