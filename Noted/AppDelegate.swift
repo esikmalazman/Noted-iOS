47f4c99acb1e4305c61a648478f284ef597ed15b
Ikmal Azman <opikmalazman@gmail.com>
2022-03-13 21:43:32 +0800

    add firebase analytics and crashlytics to dependecy

--- Noted/AppDelegate.swift
+++ Noted/AppDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 import CoreData
 import IQKeyboardManagerSwift
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         setupKeyboardManager()
         setupNavigationBarAppearance()
+        FirebaseApp.configure()
+        
+        UserDefaults.standard.set(false, forKey: "_UIConstraintBasedLayoutLogUnsatisfiable")
         return true
     }
 
