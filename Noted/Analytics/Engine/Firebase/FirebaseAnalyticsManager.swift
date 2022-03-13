df13315fced764d7ead18be8f06eb5f1cd252ee5
Ikmal Azman <opikmalazman@gmail.com>
2022-03-13 21:46:59 +0800

    create firebase analytic manager to provide api to log user event

---
+++ Noted/Analytics/Engine/Firebase/FirebaseAnalyticsManager.swift
@@ -0,0 +1,20 @@
+//
+//  FirebaseAnalyticsManager.swift
+//  Noted
+//
+//  Created by Ikmal Azman on 13/03/2022.
+//
+
+import Foundation
+/// Provide an API to log given event
+final class FirebaseAnalyticsManager {
+    private let engine: AnalyticsEngine
+    
+    init(engine : AnalyticsEngine) {
+        self.engine = engine
+    }
+    
+    func log(_ event  : AnalyticsEvent) {
+        engine.sendAnalyticsEvent(named: event.name, metadata: event.metadata)
+    }
+}
