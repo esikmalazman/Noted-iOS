667690f55075ef4af092442e9630ccb9f5e5d745
Ikmal Azman <opikmalazman@gmail.com>
2022-03-13 21:44:33 +0800

    create analytic engine protocol to allow multiple sdk analytics to be implemented

---
+++ Noted/Analytics/AnalyticsEngine.swift
@@ -0,0 +1,12 @@
+//
+//  AnalyticsEngine.swift
+//  Noted
+//
+//  Created by Ikmal Azman on 13/03/2022.
+//
+
+import Foundation
+/// Responsible to send analytics to BE and protocol that implement multiple analytics SDK with single guidelines
+protocol AnalyticsEngine : AnyObject {
+    func sendAnalyticsEvent(named name : String, metadata : [String:String])
+}
